f41cdb5df7aec3a1770fb931fa2c37944afe9a2f
desktoplinux <alvin.tan@mail.mcgill.ca>
2020-05-25 15:45:33 -0400

    Completed write benchmark for redis_throughput.js

--- redis_throughput.js
+++ redis_throughput.js
@@ -1,6 +1,7 @@
 // Redis, Redis Time Series and Poisson Process Modules
 const redis = require('redis');
 const redisTS = require('redistimeseries-js');
+const { promisify } = require('util');
 
 // Start Redis server with following options
 const redisOpt = {
@@ -10,36 +11,65 @@ const redisOpt = {
 const rCli = redis.createClient(redisOpt);
 const rtsCli = new redisTS(redisOpt);
 
-// Create a key
-const rtsKey = "rtsKey";
+
 
 
 /** TESTING REDIS TIME SERIES **/
-
-// Create key in database
+// Creates time series key in database
+const rtsKey = "rtsKey";
 var createKey = async () => {
     await rtsCli.connect();
     await rtsCli.create(rtsKey).retention(60000).send();
 }
 
+// Benchmarking time series writes
+var writeTSValues = async () => {
+    await rtsCli.connect();     // Connect client
 
-
-// Function that repeats Redis actions with specific Poisson mean rate
-var addTSValues = async () => {
-    await rtsCli.connect();
+    var promises = [];          // Array of Promises
 
     var start = Date.now();
-    for(let i=1; i< 5000; i++){
-        rtsCli.add(rtsKey, i, i).send();
+
+    // Repeatedly write data, add promises to array and then await all
+    for(let i=1; i<= 10000; i++){
+        promises.push(rtsCli.add(rtsKey, i, i).send());
     }
+    await Promise.all(promises);
 
     var end = Date.now();
 
-    var reply = await rtsCli.info(rtsKey).send();
-    console.log(end-start);
+    console.log("Time taken for time series writes: " + end-start);
+}
+
+// Testing
+createKey();
+writeTSValues();
+
+
+
+/** TESTING REDIS SORTED SETS **/
+const zaddKey = 'zaddKey';      // Create ZADD key in database
+
+// Benchmarking ZADDwrites
+var writeSSValues = async () => {
+    await rtsCli.connect();     // Connect client
+
+    var promises = [];          // Array of Promises
+    var ZADD = promisify(rCli.zadd).bind(rCli);    // Promisify "ZADD" action
+
+    var start = Date.now();
+
+    // Repeatedly write data, add promises to array and then await all
+    for(let i=1; i<= 10000; i++){
+        promises.push(ZADD(zaddKey, i, i));
+    }
+    await Promise.all(promises);
+
+    var end = Date.now();
+
+    console.log("Time taken for ZADD writes: " + end-start);
 }
 
 
 // Testing
-createKey();
-addTSValues();
+writeSSValues();
