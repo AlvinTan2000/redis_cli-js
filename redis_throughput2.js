399fcbd76b116e7ea27740d42a73efcae9420d8f
desktoplinux <alvin.tan@mail.mcgill.ca>
2020-05-26 19:08:20 -0400

    Removed the settimout()

--- redis_throughput2.js
+++ redis_throughput2.js
@@ -6,8 +6,8 @@ const SAMPLE_TIME = 1577836800;
 const HOST = 'localhost';
 const PORT = 7000;
 
+let msgout =0;
 let msgin = 0;
-let msgout = 0;
 let finished = false;
 
 // Start Redis server with following options
@@ -40,25 +40,26 @@ function writeSSValues () {
 
 // Benchmarking ZADD reads
 function readSSValues () {
-    if (!finished) {
-        rCli.zrange(zaddKey,msgout, msgout, function(error, result){
-            msgin++;
-            if (msgin >= benchmarker.BENCHMARK_ITERATIONS) {
-                finished = true;
-            }
-        })
-        msgout++;
-        setTimeout(readSSValues, 0 );
-    } else {
-        benchmarker.stopClock();
-        benchmarker.printResult(msgin);
+    for(let msgout = 0 ; msgout < benchmarker.BENCHMARK_ITERATIONS; msgout++){
+            rCli.zrange(zaddKey, msgout, msgout, function (error, result) {
+                if (msgin == 0){
+                    console.log("start");
+                    benchmarker.startClock();
+                }
+                console.log(result);
+                msgin++;
+                if (msgin >= benchmarker.BENCHMARK_ITERATIONS) {
+                    finished = true;
+                    benchmarker.stopClock();
+                    benchmarker.printResult(msgin);
+                }
+            })
     }
 }
 
 
 function main () {
-        benchmarker.startClock();
-        writeSSValues();
+        readSSValues();
 }
 
 main();
