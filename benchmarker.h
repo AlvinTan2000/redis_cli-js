c7dd23bdcd8f3e0b4805e6ce1f59298edb815c0f
AlvinTan2000 <alvin.tan@mail.mcgill.ca>
2020-05-30 12:01:27 -0400

    Added iteratively TS.ADD code

---
+++ benchmarker.h
@@ -0,0 +1,32 @@
+#ifndef REDIS_TEST_BENCHMARKER_H
+#define REDIS_TEST_BENCHMARKER_H
+
+#endif //REDIS_TEST_BENCHMARKER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#define Benchmark_timespec_init (struct timespec*)malloc(sizeof(struct timespec))
+
+#ifdef DEBUG
+static const unsigned long long BENCHMARK_ITERATIONS = 1e3;
+#else
+static const unsigned long long BENCHMARK_ITERATIONS = 1e5;
+#endif
+static const unsigned long long SAMPLE_TIME = 1577836800;
+static const unsigned long long NSEC_PER_SEC = 1e9;
+static const unsigned long long BILLION = 1e9;
+
+struct timespec begin, end, result, sum;
+
+void timespec_subtract (struct timespec*, struct timespec*, struct timespec*);
+void timespec_add (struct timespec*, struct timespec*, struct timespec*);
+
+void start_clock (void);
+void stop_clock (void);
+void restart_clock(void);
+
+void print_result (unsigned long long);
--- main.c
+++ main.c
@@ -3,13 +3,18 @@
 #include <string.h>
 
 #include <hiredis/hiredis.h>
+#include "benchmarker.h"
 
 int main(int argc, char **argv) {
     redisContext *c;
     redisReply *reply;
 
     const char *hostname = (argc > 1) ? argv[1] : "127.0.0.1";
-    int port = (argc > 2) ? atoi(argv[2]) : 6379;
+    const int port = (argc > 2) ? atoi(argv[2]) : 6379;
+
+    /* Keys for Sorted Sets and RedisTimeSeries */
+    const char *ZADDKEY = strdup("zaddkey");
+    const char *RTSKEY = strdup("rtskey");
 
     /* Connect to Redis server  */
     c = redisConnect(hostname, port);
@@ -23,15 +28,39 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    /* Let's check what we have inside the list */
-    char *ZADDKEY = strdup("zaddkey");
-    for (int i = 0; i < 1000; i++) {
+
+    /* Synchronous Writes in Sorted Sets */
+    start_clock();
+    for (int i = SAMPLE_TIME; i < BENCHMARK_ITERATIONS+SAMPLE_TIME; i++) {
         reply = redisCommand(c, "ZADD %s %d %d", ZADDKEY, i,i);
-        if (reply->type == REDIS_REPLY_ERROR) {
-            printf("%s\n", reply->str);
-        }
+//        if (reply->type == REDIS_REPLY_ERROR) {
+//            printf("%s\n", reply->str);
+//        }
         freeReplyObject(reply);
     }
+    stop_clock();
+    print_result(BENCHMARK_ITERATIONS);
+
+    restart_clock();
+
+    /* Synchronous Writes in RedisTimeSeries */
+    reply = redisCommand(c, "TS.CREATE %s", RTSKEY);
+    if (reply->type == REDIS_REPLY_ERROR) {
+        printf("%s\n", reply->str);
+    }
+
+
+    start_clock();
+    for (int j = SAMPLE_TIME; j < BENCHMARK_ITERATIONS+SAMPLE_TIME; j++) {
+        reply = redisCommand(c, "TS.ADD %s %d 100", RTSKEY, j);
+//        if (reply->type == REDIS_REPLY_ERROR) {
+//            printf("%s\n", reply->str);
+//        }
+        freeReplyObject(reply);
+    }
+    stop_clock();
+    print_result(BENCHMARK_ITERATIONS);
+
 
 /* Disconnects and frees the context */
     redisFree(c);
